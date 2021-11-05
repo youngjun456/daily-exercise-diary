b4c731ad7ed9ff3f1e6a041117d47305a9313144
youngjun456 <90443575+youngjun456@users.noreply.github.com>
2021-11-06 00:38:28 +0900

    Add files via upload

---
+++ 11월.js
@@ -0,0 +1,20 @@
+const date = new date();
+
+const month = [
+    "1월"
+    "2월"
+    "3월"
+    "4월"
+    "5월"
+    "6월"
+    "7월"
+    "8월"
+    "9월"
+    "10월"
+    "11월"
+    "12월"
+];
+
+document.querySelector(".date h1").innerHTML
+=months[3];
+
